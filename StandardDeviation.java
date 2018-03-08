9d7e59a3258fefc8ffd3d5def0815764bf1245af
Samuel Bagley <sam.bagley@outlook.com>
2018-03-08 11:28:09 +0000

    initial commit

---
+++ StandardDeviation.java
@@ -0,0 +1,32 @@
+public class StandardDeviation
+{
+	public static void main(String[] args)
+	{
+        double[] x = new double[] { -5, 1, 8, 7, 2 };
+        int n =5;
+        double mean = 0;
+        double StdDev = 0;
+
+        //calculate mean
+        for (int i = 0; i <= (n-1); i++)
+        {
+            mean = mean + x[i];
+			System.out.println(x[i]);
+        }
+
+        mean = mean / n;
+
+        for (int i = 0; i <= (n-1); i++)
+        {
+            StdDev = StdDev + (mean - x[i]) * (mean - x[i]);
+        }
+
+        StdDev = StdDev / n;
+
+		System.out.println("Variance: " + StdDev);
+
+		StdDev = Math.sqrt(StdDev);
+
+		System.out.println("Standard Deviation: " + StdDev);
+	}
+}
